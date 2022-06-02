ff09b3de9f34b6c65b09a892153c1c4f62472326
Kristupas Gaidys <kristupas@benme.io>
2022-06-02 19:25:46 +0300

    Fixed tele config
    :

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,10 +34,10 @@ require("packer").startup(function(use)
 	})
 
 	use({
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			run = "make",
-		},
+		"nvim-telescope/telescope-fzf-native.nvim",
+		run = "make",
+	})
+	use({
 		"nvim-telescope/telescope.nvim",
 		requires = "nvim-lua/plenary.nvim",
 		config = function()
