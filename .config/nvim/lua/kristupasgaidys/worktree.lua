8c4ad976840d9b97ae4bf054d3a94ee00faea020
Kristupas Gaidys <kristupas@benme.io>
2022-08-15 17:00:41 +0300

    Start working git worktree workflow

--- .config/nvim/lua/kristupasgaidys/worktree.lua
+++ .config/nvim/lua/kristupasgaidys/worktree.lua
@@ -1,6 +1,30 @@
-require("git-worktree").setup({})
+local worktree = require("git-worktree")
 
-require("telescope").load_extension("git_worktree")
+worktree.my_create_git_worktree = function ()
+  local branch_name = vim.fn.input("Branch name > ")
+  worktree.create_worktree(".worktree", "main", branch_name)
+end
 
-Normal("<leader>gw", require("telescope").extensions.git_worktree.git_worktrees, "Switch worktree")
-Normal("<leader>gc", require("telescope").extensions.git_worktree.create_git_worktree, "Create worktree")
+worktree.setup({})
+local telescope = require("telescope")
+telescope.load_extension("git_worktree")
+
+-- op = Operations.Switch, Operations.Create, Operations.Delete
+-- metadata = table of useful values (structure dependent on op)
+--      Switch
+--          path = path you switched to
+--          prev_path = previous worktree path
+--      Create
+--          path = path where worktree created
+--          branch = branch name
+--          upstream = upstream remote name
+--      Delete
+--          path = path where worktree deleted
+worktree.on_tree_change(function(op, metadata)
+  if op == worktree.Operations.Switch then
+    print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+  end
+end)
+
+Normal("<leader>gw", telescope.extensions.git_worktree.git_worktrees, "Switch worktree")
+Normal("<leader>gc", worktree.my_create_git_worktree, "Create worktree")
