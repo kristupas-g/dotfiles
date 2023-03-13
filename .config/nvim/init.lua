7ac7de4ed0b0587826c9366bc1f9da39c95ff313
Kristupas Gaidys <kristupas@benme.io>
2023-03-13 19:19:18 +0200

    Clean up init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,6 @@ require("config.keybinds")
 require("config.autocmds")
 require("config.statusline")
 
--- This will leave later
-require("personal.lua_locals").setup()
-
 require("lazy").setup("plugins", require("config.lazy"))
+
+vim.cmd.colorscheme("catppuccin")
--- .config/nvim/lua/telescope/_extensions/command_picker.lua
+++
@@ -1,8 +0,0 @@
-return require("telescope").register_extension({
-  setup = function(ext_config, config)
-    -- return vim.tbl_deep_extend("force", config, ext_config or {})
-  end,
-  exports = {
-    command_picker = require("personal.lua_locals.telescope").get_command,
-  },
-})
