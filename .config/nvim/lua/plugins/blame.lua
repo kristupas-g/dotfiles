043e7118ac0104412593aa874f34204c1d1966ad
Kristupas Gaidys <kristupas.gaidys@vinted.com>
2023-06-21 12:17:34 +0300

    other

---
+++ .config/nvim/lua/plugins/blame.lua
@@ -0,0 +1,6 @@
+return {
+  "f-person/git-blame.nvim",
+  config = function()
+    vim.g.gitblame_enabled = 1
+  end,
+}
---
+++ .config/nvim/lua/plugins/rails.lua
@@ -0,0 +1,9 @@
+return {
+  { "tpope/vim-rails" },
+  {
+    "weizheheng/ror.nvim",
+    config = function()
+      vim.keymap.set("n", "<Leader>rc", ":lua require('ror.commands').list_commands()<CR>", { silent = true })
+    end,
+  },
+}
