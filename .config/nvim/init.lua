9ffbf427c28bb24c2f164b33ff35d55f56b8e704
Kristupas Gaidys <kristupas.gaidys@vinted.com>
2024-11-12 13:23:52 +0200

    improvements everywhere

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -108,6 +108,43 @@ require('lazy').setup({
   },
 
   {
+    'ggandor/leap.nvim',
+    config = function()
+      require('leap').create_default_mappings()
+    end,
+  },
+
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require('harpoon')
+      harpoon:setup()
+
+      vim.keymap.set('n', 'M', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', 'mm', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set('n', 'ma', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', 'ms', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', 'md', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', 'mf', function()
+        harpoon:list():select(4)
+      end)
+    end,
+  },
+
+  {
     'stevearc/oil.nvim',
     lazy = false,
     opts = {
@@ -157,7 +194,7 @@ require('lazy').setup({
             i = {
               ['<esc><esc>'] = false,
               ['jk'] = actions.close,
-            }
+            },
           },
         },
         extensions = {
@@ -205,6 +242,12 @@ require('lazy').setup({
       enabled = true,
       trigger_events = { 'InsertLeave' },
       execution_message = { message = '' },
+      condition = function(buf)
+        if vim.bo[buf].filetype == "harpoon" then
+          return false
+        end
+        return true
+      end
     },
   },
 
