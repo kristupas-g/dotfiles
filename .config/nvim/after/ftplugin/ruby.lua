bb595293536374baa02a06d7113448f080429ee3
Kristupas Gaidys <kristupas.gaidys@vinted.com>
2024-12-30 16:18:43 +0200

    run line spec

--- .config/nvim/after/ftplugin/ruby.lua
+++ .config/nvim/after/ftplugin/ruby.lua
@@ -7,3 +7,13 @@ vim.keymap.set('n', '<leader>t', function()
   Tmux_split('bundle exec rspec ' .. test_file_path)
 end, { noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>T', function()
+  local current_file = vim.fn.expand('%:.')
+  local test_file_path = current_file:find('_spec.rb') and current_file or vim.b.onv_otherFile
+  if test_file_path == nil then
+    vim.notify('Could not find test file')
+  end
+  local line_no, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  Tmux_split('bundle exec rspec ' .. test_file_path .. ':' .. line_no)
+end, { noremap = true, silent = true })
+
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -135,6 +135,7 @@ vim.api.nvim_create_user_command('Gh', OpenInGH, {})
 vim.keymap.set('n', '<leader>p', '<C-^>')
 vim.keymap.set('n', '<leader>qp', ':cprevious<CR>')
 vim.keymap.set('n', '<leader>qn', ':cnext<CR>')
+vim.keymap.set('n', '<leader>qq', ':copen<CR>')
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
